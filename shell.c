0fd1180d7fd9bc683198ea5e1b9a00a3205c47e6
Argen-tal <carlpeacebringer@gmail.com>
2023-10-18 12:14:20 -0700

    betty error fixed

--- shell.c
+++ shell.c
@@ -1,69 +1,114 @@
 #include "shell.h"
 
 /**
-* main - Entry point od the simple shell program
-* @argc: The number of arguments
-* @argv: The array of arguments
-* @env: The array of environment variables
-* Return: 0 on success, or the exit status of the last command executed
-*/
+ * main - Entry point of the simple shell program
+ * @argc: The number of arguments
+ * @argv: The array of arguments
+ * @env: The array of environment variables
+ * Return: 0 on success, or the exit status of the last command executed
+ */
 int main(int argc, char **argv, char **env)
 {
-char *line = NULL;
-size_t len = 0;
-ssize_t nread;
-char *token;
-char **args;
-int i, status;
-(void)argc;
-(void)argv;
-signal(SIGINT, sigint_handler);
-while (1)
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    char *token;
+    char **args;
+    int i, status;
+
+    (void)argc;
+    (void)argv;
+
+    signal(SIGINT, sigint_handler);
+
+    while (1) {
+        if (isatty(STDIN_FILENO)) {
+            write(STDOUT_FILENO, "$ ", 2);
+        }
+
+        nread = getline(&line, &len, stdin);
+        if (nread == -1) {
+            if (isatty(STDIN_FILENO)) {
+                write(STDOUT_FILENO, "\n", 1);
+            }
+            free(line);
+            exit(0);
+        }
+
+        line[nread - 1] = '\0';
+
+        args = malloc(sizeof(char *) * 32);
+        if (args == NULL) {
+            free(line);
+            exit(1);
+        }
+
+        token = strtok(line, " \t\n\r");
+        i = 0;
+        while (token != NULL) {
+            args[i] = token;
+            i++;
+            token = strtok(NULL, " \t\n\r");
+        }
+        args[i] = NULL;
+
+        if (_strcmp(args[0], "exit") == 0) {
+            free(line);
+            free(args);
+            exit(0);
+        }
+
+        if (_strcmp(args[0], "env") == 0) {
+            print_env(env);
+            free(line);
+            free(args);
+            continue;
+        }
+
+        status = execute_cmd(args, env);
+        if (status == -1) {
+            perror("Error");
+        }
+
+        free(args);
+    }
+
+    free(line);
+    return (status);
+}
+
+/**
+ * execute_cmd - Execute a command.
+ * @args: The array of arguments.
+ * @env: The array of environment variables.
+ * Return: 0 on success, or -1 on failure.
+ */
+int execute_cmd(char **args, char **env)
 {
-if (isatty(STDIN_FILENO))
-    write(STDOUT_FILENO, "$ ", 2);
-nread = getline(&line, &len, stdin);
-if (nread == -1)
+    /* Find the command path.*/
+    char *path = find_path(args[0], env);
+    if (path == NULL) {
+        return -1;
+    }
+
+    /*Execute the command.*/
+    execve(path, args, env);
+
+    /* If we reach here, something went wrong.*/
+    perror("execve");
+    return -1;
+}
+
+/**
+ * find_path - Find the path to a command.
+ * @cmd: The command name.
+ * @env: The array of environment variables.
+ * Return: The path to the command, or NULL if the command cannot be found.
+ */
+char *find_path(char *cmd, char **env)
 {
-if (isatty(STDIN_FILENO))
-    write(STDOUT_FILENO, "\n", 1);
-free(line);
-exit(0);
+
+    /*Return the path to the command, or NULL if the command cannot be found.*/
+    return path;
 }
-line[nread - 1] = '\0';
-args = malloc(sizeof(char *) * 32);
-if (args == NULL)
-{
-free(line);
-exit(1);
-}
-token = strtok(line, " \t\n\r");
-i = 0;
-while (token != NULL)
-{
-args[i] = token;
-i++;
-token = strtok(NULL, "\t\n\r");
-}
-args[i] = NULL;
-if (_strcmp(args[0], "exit") == 0)
-{
-free(line);
-free(args);
-exit(0);
-}
-if (_strcmp(args[0], "env") == 0)
-{
-print_env(env);
-free(line);
-free(args);
-continue;
-}
-status = execute(args, env);
-if (status == -1)
-    perror("Error");
-free(args);
-}
-free(line);
-return (status);
-}
+
--- utils.c
+++ utils.c
@@ -1,64 +1,60 @@
 #include "shell.h"
 
 /* _getenv: Get the value of an environment variable */
-char *_getenv(const char *name, char **env)
-{
-int i;
-int len;
+char *_getenv(const char *name, char **env) {
+	int i;
+	int len;
 
-len = strlen(name);
-for (i = 0; env[i] != NULL; i++)
-{
-if (strncmp(env[i], name, len) == 0 && env[i][len] == '=')
-  return (env[i] + len + 1);
-}
-return (NULL);
+	len = strlen(name);
+	for (i = 0; env[i] != NULL; i++) {
+		if (strncmp(env[i], name, len) == 0 && env[i][len] == '=') {
+			return (env[i] + len + 1);
+		}
+	}
+	return (NULL);
 }
 
 /* _strlen: Get the length of a string */
-size_t _strlen(const char *s)
-{
-size_t len;
+size_t _strlen(const char *s) {
+	size_t len;
 
-len = 0;
-while (*s != '\0')
-{
-len++;
-s++;
-}
-return (len);
+	len = 0;
+	while (*s != '\0') {
+		len++;
+		s++;
+	}
+	return (len);
 }
 
 /* _strcpy: Copy a string to another string */
-char *_strcpy(char *dest, const char *src)
-{
-  char *p;
-p = dest;
-while (*src != '\0')
-{
-*dest = *src;
-dest++;
-src++;
-}
-*dest = '\0';
-return (p);
+char *_strcpy(char *dest, const char *src) {
+	char *p;
+
+	p = dest;
+	while (*src != '\0') {
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (p);
 }
 
 /* _strcat: concatenates two strings */
-char *_strcat(char *dest, const char *src)
-{
-  char *p;
-p = dest;
-while (*dest != '\0')
-  dest++;
-while (*src != '\0')
-{
-*dest = *src;
-dest++;
-src++;
-}
-*dest = '\0';
-return (p);
+char *_strcat(char *dest, const char *src) {
+	char *p;
+
+	p = dest;
+	while (*dest != '\0') {
+		dest++;
+	}
+	while (*src != '\0') {
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (p);
 }
 
 /**
@@ -67,14 +63,14 @@ return (p);
 * @s2: second string
 * Return: difference between the first unmatched characters or 0 if equal
 */
-int _strcmp(const char *s1, const char *s2)
-{
-  while (*s1 && s2)
-{
-if (*s1 != *s2)
-  return (*s1 - *s2);
-s1++;
-s2++;
+int _strcmp(const char *s1, const char *s2) {
+	while (*s1 && *s2) {
+		if (*s1 != *s2) {
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
-return (*s1 - *s2)
-}
+
